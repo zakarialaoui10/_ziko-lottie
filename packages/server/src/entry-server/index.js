d2da753cf4afecd5640da63c405c98bba8b8d059
zakarialaoui10 <zakarialaoui10@gmail.com>
2026-09-07 13:43:55 +0100

    move entry server logic to @zikojs/server

---
+++ packages/server/src/entry-server/index.js
@@ -0,0 +1,21 @@
+import { globImports } from '../server-only-utils/index.js'
+import { createFileBasedRouter } from 'ziko/app/router'
+
+export function createEntryServer(){
+    return async function render(_url) {
+        const pages = await globImports()
+        const app = await createFileBasedRouter({
+            pages,
+            renderer : async (target, component, props, wrapper) => {
+            console.log(component(props))
+            },
+            target : 'no dom',
+            url : _url
+        })
+        console.log(app)
+        const html = `
+            <h1> Hello from server </h1>
+        `
+        return { html }
+    }
+}
